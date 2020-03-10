fc5e3bbbbe95db8a574deb27d453c5e619ab03cf
snsaleh1 <snsaleh11@gmail.com>
2020-03-10 00:30:50 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is dope!
+          This is the bomb diggity doe!
         </p>
         <a
           className="App-link"
